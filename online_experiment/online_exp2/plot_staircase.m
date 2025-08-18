b8c838e19a94a2252888068a33a1eb3a04b8361d
Helen <fh986@nyu.edu>
2025-08-18 12:50:35 -0400

    plot staircase for each file

--- online_experiment/online_exp2/plot_staircase.m
+++ online_experiment/online_exp2/plot_staircase.m
@@ -5,11 +5,11 @@
 clear all;
 %close all;
 
-addpath('/Users/helenhu/Documents/MATLAB/Gaze_Package')
+% addpath('/Users/helenhu/Documents/MATLAB/Gaze_Package')
 
 %% set up files
 
-mydir = pwd;
+mydir = [pwd '/online2_data'];
 
 d = dir(sprintf('%s/*.csv',mydir));
 
@@ -29,33 +29,28 @@ numSubj = length(mainFiles);
 %% 
 
 eccentricity = 10;
+
 for subj = 1:numSubj
 
     mainOutput = readtable([mydir filesep mainFiles{subj}]);
     ntrials = 35;
-    cond_seq = {'Stationary Right','Stationary Left', ...
-        'Dynamic Right','Dynamic Left','Flies Right','Flies Left'};
-    lineColors = {'#7E2F8E','#D95319','#0072BD'};
+    cond_seq = {'Stationary_Right','Stationary_Left', ...
+        'Dynamic_Right','Dynamic_Left','Flies_Right','Flies_Left'};
+    lineColors = {'#7E2F8E','#7E2F8E','#D95319','#D95319','#0072BD','#0072BD'};
     disp(mainFiles{subj})
 
     figure;clf
     hold on;
     ct = 1;
-    for b = 7:9 % blocks 
-    
-        for s = 1:2 % 1 = right threshold, 2 = left threshold
-    
-            blockOfInterest = b;
-            staircase = s;
+    for condition = 1:length(cond_seq)
 
-            t_block_cond = mainOutput(contains(mainOutput.staircaseName,sprintf('%i_%i',blockOfInterest,staircase)),:);
-            h(ct) = plot(10.^t_block_cond.questMeanBeforeThisTrialResponse,'Color',cell2mat(lineColors(rem(b,3)+1)),'LineWidth',2);
-            ct = ct + 1;
-            
-            bouma = 10 ^ t_block_cond.questMeanAtEndOfTrialsLoop(end)/eccentricity;
-            disp(bouma)
+        t_block_cond = mainOutput(strcmp(mainOutput.conditionName,cond_seq(condition)),:);
+        h(ct) = plot(10.^t_block_cond.questMeanBeforeThisTrialResponse,'Color',cell2mat(lineColors(ct)),'LineWidth',2);
+        ct = ct + 1;
+        
+        bouma = 10 ^ t_block_cond.questMeanAtEndOfTrialsLoop(end)/eccentricity;
+        disp(bouma)
     
-        end
     end
     h(2).LineStyle = '-.';
     h(4).LineStyle = '-.';
