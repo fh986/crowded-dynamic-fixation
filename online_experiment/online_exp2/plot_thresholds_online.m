116fa7af6f1bfc81008dfb642a6d614d76b4db19
Helen <fh986@nyu.edu>
2025-08-18 18:10:31 -0400

    changed axis limits

--- online_experiment/online_exp2/plot_thresholds_online.m
+++ online_experiment/online_exp2/plot_thresholds_online.m
@@ -201,7 +201,7 @@ table_stationary = results_for_plotting(strcmp(results_for_plotting.Condition, '
 table_dynamic = results_for_plotting(strcmp(results_for_plotting.Condition, 'Dynamic'),:);
 table_flies = results_for_plotting(strcmp(results_for_plotting.Condition, 'Flies'),:);
 
-ylimit = [0 5];
+ylimit = [0 8];
 % bwidth = 0.1;
 xlimit = [0.01 1.2];
 
